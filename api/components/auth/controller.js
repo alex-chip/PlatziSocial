a47c53875fbabe0956f9e12f4ed4c092989b1bba
Alex Chip <alex-chip@hotmail.com>
2020-01-23 20:38:11 -0600

    cifrando password

--- api/components/auth/controller.js
+++ api/components/auth/controller.js
@@ -1,5 +1,9 @@
+// libreria para sifrar contraseñas
+const bcrypt = require('bcrypt')
+
 // requerir el archico auth donde se genera el TOKEN
 const auth = require('../../../auth')
+
 const TABLA = 'auth'
 module.exports = (injectedStore) => {
   let store = injectedStore
@@ -10,17 +14,22 @@ module.exports = (injectedStore) => {
   // Funcion para loguear al usuario
   async function login (username, password) {
     const data = await store.query(TABLA, { username: username})
-    if (data.password === password) {
-      // generar token, y se firma la dara recibida
-      return auth.sign(data)
-    } else {
-      // caso contrario generar un error
-      throw new Error('Informacion invalida')
-    }
-    return data
+    
+    // comparar el password, se retorna el token si es true o un error si es falso
+    return bcrypt.compare(password, data.password)
+      .then(sonIguales => {
+        if (sonIguales === true) {
+          // generar token, y se firma la data recibida
+          return auth.sign(data)
+        } else {
+          // caso contrario generar un error
+          throw new Error('Informacion invalida')
+        }
+      })
+    // return data
   }
 
-  function upsert (data) {
+  async function upsert (data) {
     const authData = {
       id: data.id
     }
@@ -30,7 +39,9 @@ module.exports = (injectedStore) => {
     }
 
     if (data.password) {
-      authData.password = data.password
+      // authData.password = data.password
+      // cifrando el password
+      authData.password = await bcrypt.hash(data.password, 8)
     }
 
     return store.upsert(TABLA, authData)
